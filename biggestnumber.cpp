74b814952cf51dec20e9ac01578be41c92f3df6c
cristinapereez <cperez58@mail.csuchico.edu>
2018-02-06 11:43:51 -0800

    Add in/outputs

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Cristina Perez
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
@@ -7,12 +7,22 @@ int main()
 {
 int wholeNumber1;
 int wholeNumber2;
+int biggest;
   cout<<"Please enter a whole number:\n";
   cin>>wholeNumber1;
   cout<<"Please enter another whole number:\n";
   cin>>wholeNumber2;
+  
+  if(wholeNumber1>wholeNumber2)
+  {
+    biggest=wholeNumber1;
+    }
+    else
+    {
+    biggest=wholeNumber2;
+    }
 
-  cout<<"Of those two numbers, the biggest is: ";
+  cout<<"Of those two numbers, the biggest is: "<<biggest<<endl;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
