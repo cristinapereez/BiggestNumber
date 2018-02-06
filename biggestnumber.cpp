4d38e354b682adbfbdeef2a3bb55a0bb90a12aa8
cristinapereez <cperez58@mail.csuchico.edu>
2018-02-06 11:21:36 -0800

    Adds Inputs

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,10 +5,12 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+int wholeNumber1;
+int wholeNumber2;
   cout<<"Please enter a whole number:\n";
-  
+  cin>>wholeNumber1;
   cout<<"Please enter another whole number:\n";
+  cin>>wholeNumber2;
 
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
